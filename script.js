86d73659d4d04886b2c4026d04cec2e23b59122a
Nilesh Keshari <kesharinilesh13@gmail.com>
2023-09-09 20:35:36 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-var crsr = document.querySelector("#cursor");
+// var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function(dets){
-    crsr.style.left=dets.x-10+"px";
-    crsr.style.top=dets.y-10+"px";
-    blur.style.left=dets.x-140+"px";
-    blur.style.top=dets.y-140+"px";
+    // crsr.style.left=dets.x-10+"px";
+    // crsr.style.top=dets.y-10+"px";
+    blur.style.left=dets.x-180+"px";
+    blur.style.top=dets.y-180+"px";
     
 });
 
