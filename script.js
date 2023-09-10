1d886208259b218a485a65586f664a69dd5e5416
Nilesh Keshari <kesharinilesh13@gmail.com>
2023-09-10 11:21:02 +0530

    commit

--- script.js
+++ script.js
@@ -1,37 +1,114 @@
-
 var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function(dets){
-    crsr.style.left=dets.clientX-10+"px";
-    crsr.style.top=dets.clientY-10+"px";
-    blur.style.left=dets.x-180+"px";
-    blur.style.top=dets.y-180+"px";
-    // dets.clientX=dets.clientX+"px";
-    // dets.clientY=dets.clientY+"px";
+document.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.clientX-10 + "px";
+    crsr.style.top = dets.clientY -10+ "px";
+    blur.style.left = dets.x - 165 + "px";
+    blur.style.top = dets.y - 165 + "px";
 });
 
-gsap.to("#nav",{
-    backgroundColor:"#000",
-    duration:0.5,
-    height:"110px",
-    scrollTrigger:{
-        trigger:"#nav",
-        scroller:"body",
-        // markers:true,
-        start:"top -10%",
-        end:"top -11%",
-        scrub:1
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+    });
+    elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+    });
+});
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 2,
     },
 });
-gsap.to("#main",{
-    backgroundColor:"#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        // markers:true,
-        start:"top -25%",
-        end:"top -75%",
-        scrub:2
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
     },
 });
+
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
+    },
+});
+gsap.from(".card", {
+    scale: 0.8,
+    opacity:0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers:false,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+    },
+});
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+    },
+});
+gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+    },
+});
+gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+    },
+});
+
+// Thanks itna aage tak aane ke liye lekin pura code utha ke copy paste karne ki jagah khud ek baar banane ka try karna, kuch naya seekhne ko milega!
